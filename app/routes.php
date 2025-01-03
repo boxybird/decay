b048d8d7c59c953f2cf38904e38d6683cd97d440
Andrew Rhyand <andrew.rhyand@gmail.com>
2025-01-03 07:57:16 -0700

    Remove hardcoded movie limit in movie filtering logic

--- app/routes.php
+++ app/routes.php
@@ -7,7 +7,7 @@ Flight::route('/', function () {
     $movie_id = (int) $_GET['movie_id'] ??= 23389;
     $member_id = (int) $_GET['member_id'] ??= false;
     $has_member_id = (bool) $member_id ??= false;
-    
+
     $db = Flight::db();
 
     $single_movie = $db->findOneBy(['data.id', '=', $movie_id])['data'] ?? null;
@@ -16,7 +16,6 @@ Flight::route('/', function () {
     $movies = array_map(fn($movie) => MovieData::fromArray($movie['data']), $db->findAll());
     $movies = array_filter($movies, fn($movie) => $movie->id !== $single_movie->id);
     $movies = array_filter($movies, fn($movie) => $movie->computed->poster_paths);
-    $movies = array_slice($movies, 0, 30);
 
     $member = array_filter($single_movie->credits->cast, fn($member) => $member->id == $member_id) ?? null;
     $member = $member ? array_values($member)[0] : null;
