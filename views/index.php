20c77cf544c038525e53a31dcc82f31f9529a2b1
Andrew Rhyand <andrew.rhyand@gmail.com>
2025-01-05 11:21:56 -0700

    Refactor variable usage in movie details view

--- views/index.php
+++ views/index.php
@@ -6,7 +6,7 @@
                     class="duration-150 h-full inline-flex object-cover w-24 [&.htmx-request]:animate-pulse hover:scale-105 lg:w-full"
                     href="/?movie_id=<?= $movie->id ?>">
                     <img
-                        style="<?= ($movie->id !== $single_movie->id) ? 'view-transition-name: image-'.$movie->id : '' ?>"
+                        style="<?= ($movie->id !== $movie_single->id) ? 'view-transition-name: image-'.$movie->id : '' ?>"
                         class="rounded"
                         src="<?= $movie->computed->poster_paths['w500'] ?>"
                         alt="<?= $movie->title ?> poster"
@@ -19,51 +19,53 @@
         <div class="grid place-items-center sticky top-0 lg:h-dvh lg:pr-12">
             <article class="gap-8 flex flex-col flex-col-reverse items-center max-w-lg mx-auto lg:gap-10 lg:flex-row lg:max-w-4xl xl:max-w-5xl">
                 <img
-                    style="view-transition-name: image-<?= $has_member_id ? $member->id : $single_movie->id ?>"
+                    style="view-transition-name: image-<?= $has_member_id ? $member->id : $movie_single->id ?>"
                     class="h-full rounded-md shrink-0 lg:w-2/5"
-                    src="<?= $has_member_id ? $member->computed->profile_paths['w500'] : $single_movie->computed->poster_paths['w500'] ?>"
-                    alt="<?= $single_movie->title ?> poster"
+                    src="<?= $details['featured_image'] ?>"
+                    alt="<?= $details['featured_image_alt'] ?>"
                 >
-                <div
-                    style="view-transition-name: title-<?= $single_movie->id ?>"
-                    class="_lg:pb-10">
+                <div style="view-transition-name: title-<?= $movie_single->id ?>">
                     <h2 class="max-w-sm text-4xl">
-                        <?= $single_movie->title ?>
+                        <?= $movie_single->title ?>
                     </h2>
-                    <div class="bg-slate-900/50 mt-8 p-4 rounded-md">
-                        <p class="line-clamp-3 xl:line-clamp-5">
-                            <?= $single_movie->overview ?>
-                        </p>
-                    </div>
-                    <div class="gap-2 flex flex-wrap mt-4">
-                        <?php foreach ($single_movie->genres as $genre): ?>
-                            <span class="bg-slate-700 px-2 py-0.5 rounded-md text-sm">
-                                <?= $genre->name ?>
-                            </span>
-                        <?php endforeach ?>
-                    </div>
+                    <?php if ($has_member_id): ?>
+                        <div class="gap-2 inline-flex mt-8">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mt-[3px] -rotate-90 shrink-0 size-6 lg:rotate-0">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                            </svg>
+                            <h3 class="text-xl">
+                                <span><?= $member->name ?></span>
+                                <?php if ($member->character): ?>
+                                    <span class="text-slate-400">(<?= $member->character ?>)</span>
+                                <?php endif ?>
+                            </h3>
+                        </div>
+                    <?php endif ?>
+                    <?php if ($details['description']): ?>
+                        <div class="bg-slate-900/50 mt-8 p-4 rounded-md">
+                            <p class="line-clamp-3 xl:line-clamp-5">
+                                <?= $details['description'] ?>
+                            </p>
+                        </div>
+                    <?php endif ?>
+                    <?php if ($details['type'] === 'movie'): ?>
+                        <div class="gap-2 flex flex-wrap mt-4">
+                            <?php foreach ($movie_single->genres as $genre): ?>
+                                <span class="bg-slate-700 px-2 py-0.5 rounded-md text-sm">
+                                    <?= $genre->name ?>
+                                </span>
+                            <?php endforeach ?>
+                        </div>
+                    <?php endif ?>
                     <hr class="border-slate-900 mt-8">
                     <div class="flex flex-col flex-col-reverse lg:flex-col">
-                        <?php if ($has_member_id): ?>
-                            <div class="gap-2 inline-flex mt-8">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mt-[3px] -rotate-90 shrink-0 size-6 lg:rotate-0">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                                </svg>
-                                <h3 class="text-xl">
-                                    <span><?= $member->name ?></span>
-                                    <?php if ($member->character): ?>
-                                        <span class="text-slate-400">(<?= $member->character ?>)</span>
-                                    <?php endif ?>
-                                </h3>
-                            </div>
-                        <?php endif ?>
                         <?php if ($members): ?>
                             <ul class="gap-4 grid grid-cols-4 max-w-sm mt-8">
                                 <?php foreach ($members as $index => $member): ?>
                                     <li>
                                         <a
                                             class="duration-150 inline-flex hover:scale-105 [&.htmx-request]:animate-pulse"
-                                            href="/?movie_id=<?= $single_movie->id ?>&member_id=<?= $member->id ?>">
+                                            href="/?movie_id=<?= $movie_single->id ?>&member_id=<?= $member->id ?>">
                                             <img
                                                 style="view-transition-name: image-<?= $member->id ?>"
                                                 class="rounded"
@@ -73,12 +75,12 @@
                                 <?php endforeach ?>
                                 <?php if ($has_member_id): ?>
                                     <li>
-                                        <a href="/?movie_id=<?= $single_movie->id ?>">
+                                        <a href="/?movie_id=<?= $movie_single->id ?>">
                                             <img
-                                                style="<?= $movie->id !== $single_movie->id ? 'view-transition-name: image-'.$single_movie->id : '' ?>"
+                                                style="<?= $movie->id !== $movie_single->id ? 'view-transition-name: image-'.$movie_single->id : '' ?>"
                                                 class="duration-150 inline-flex rounded hover:scale-105"
-                                                src="<?= $single_movie->computed->poster_paths['w500'] ?>"
-                                                alt="<?= $single_movie->title ?> poster"
+                                                src="<?= $movie_single->computed->poster_paths['w500'] ?>"
+                                                alt="<?= $movie_single->title ?> poster"
                                             >
                                         </a>
                                     </li>
